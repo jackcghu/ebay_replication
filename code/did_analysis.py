df6e76061409dc99cde91ad354856a8c1ac337c8
Jack C <jackchepin25@gmail.com>
2026-02-28 14:41:34 -0800

    Add script description to DID analysis

--- code/did_analysis.py
+++ code/did_analysis.py
@@ -75,3 +75,7 @@ with open('output/tables/did_table.tex', 'w') as f:
     f.write(latex)
 
 print("\nLaTeX table written to output/tables/did_table.tex")
+# DID Analysis Script
+# Estimates the average treatment effect of turning off eBay's paid search.
+# Uses preprocessed pivot tables from preprocess.py.
+# Output: LaTeX table in output/tables/did_table.tex
